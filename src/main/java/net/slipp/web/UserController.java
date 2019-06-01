8b6ee8b8c95c2d9da11d8fafebe9b43a7e1b84b7
jimmy <blackpearl1210@gmail.com>
2019-06-01 17:07:45 +0900

    회원가입, 사용자 목록 추가"

---
+++ src/main/java/net/slipp/web/UserController.java
@@ -0,0 +1,27 @@
+package net.slipp.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	private List<User> users = new ArrayList<User>();
+	
+	@GetMapping("/create")
+	public String create(User user) {
+		System.out.println("user : " + user);
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users", users);
+		return "list";
+	}
+}
